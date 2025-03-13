75c5602e9d58e8e006606965018cc400cbc6a4d2
matei <matei.burghiu@nlenau.ro>
2025-03-13 20:06:42 +0200

    edited

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu-button");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuButton.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
